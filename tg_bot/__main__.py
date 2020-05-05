d20225901da54397bef359fc9c8ebc575aa3f0ff
krishkiran <62176063+krishkiran@users.noreply.github.com>
2020-05-06 03:24:02 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -26,6 +26,8 @@ For more commands click /help...
 
 HELP_STRINGS = """
 
+Hello! my name *{}*.
+
 *Main* available commands:
  - /start: Start the bot...
  - /help: help....
@@ -37,6 +39,12 @@ HELP_STRINGS = """
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
+
+DONATE_STRING = """Heya, glad to hear you want to donate!
+It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
+motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
+(see his bio!). He's just a poor student, so every little helps!
+There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
