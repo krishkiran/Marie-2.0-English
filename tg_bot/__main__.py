5473ff598f53a5e803db88cffde876371a49f564
krishkiran <62176063+krishkiran@users.noreply.github.com>
2020-05-26 22:38:55 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, I'm *Shreya v2.0!* I'm Group Manager [@Vellithiras😎✌️](t.me//vellithiras), Controlled By  [This Person](https://t.me/@Sree332) 😎
+Hi {}, I'm *Shreya v2.0!* I'm Group Manager [@Vellithiras](t.me//vellithiras), Controlled By  [This Person](https://t.me/Sree332) 😎
 
 For more commands click /help...
 
