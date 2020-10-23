724b4bae678a9bb244e4f9871a58d2156c38c32a
krishkiran <62176063+krishkiran@users.noreply.github.com>
2020-10-24 00:10:27 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,8 +19,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 Hi {}, I'm *Rashmika v2.0!* I'm Group Manager [@2kTamilmovies](t.me//TwokTamilMovies), Controlled By  [This Person](https://t.me/DChoco_Boy) 😎
-parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="Group", url="https://t.me/Cinema_Premikalg"), InlineKeyboardButton(text="Channel", url="https://t.me/joinchat/AAAAAFJ-W5-qaOF5XshtCw")]]))
+
 
 """
 
